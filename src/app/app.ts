4600940edb62b50e42f6ca133e269b8d2f717eaf
pluem46 <65011212155@msu.ac.th>
2026-07-07 09:19:42 +0700

    up

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-root',
   imports: [CommonModule, RouterOutlet, Header],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
